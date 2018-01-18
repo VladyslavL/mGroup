d53738f8a6ce7d76e2845fb52b9f7d4178ca2ac7
Vladyslav Liashenko <vladyslav.liashenko@gmail.com>
2018-01-18 15:22:12 +0200

    gulpfile and packages is changed

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,64 @@ var gulp = require('gulp'),
     fileInclude = require('gulp-file-include'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    image = require('gulp-image');
 
 
 gulp.task('html', function() {
-  gulp.src("./src/**/*.html")
-    .pipe(plumber())
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@root'
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.stream());
+  gulp.src(["./src/**/*.html", "!./src/fonts/**"])
+      .pipe(plumber())
+      .pipe(fileInclude({
+        prefix: '@@',
+        basepath: '@root'
+      }))
+      .pipe(gulp.dest('./dist/'))
+      .pipe(browserSync.stream());
 })
 
 gulp.task('style', function () {
-  gulp.src('./src/sass/**/*.scss')
-  .pipe(plumber())
-  .pipe(sourcemaps.init())
-  // .pipe(autoprefixer({
-  //   browsers: ['last 2 versions'],
-  //   cascade: false
-  // }))
-  .pipe(sass())  
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./dist/css/'))
-  .pipe(browserSync.stream());
+  gulp.src(['./src/sass/*.scss'])
+      .pipe(plumber())
+      .pipe(fileInclude({
+        prefix: '@@',
+        basepath: '@root'
+      }))
+      .pipe(sourcemaps.init())
+      // .pipe(autoprefixer({
+      //   browsers: ['last 2 versions'],
+      //   cascade: false
+      // }))
+      .pipe(sass())  
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('script', function() {
   gulp.src(['./src/**/*.js'])
-    .pipe(plumber())
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(browserSync.stream());
+      .pipe(plumber())
+      .pipe(gulp.dest('./dist/js/'))
+      .pipe(browserSync.stream());
 })
 
-gulp.task('serve', ['html','style','script'], function() {
+gulp.task('images', function(){
+  gulp.src(['./src/images/*'])
+      .pipe(plumber())
+      .pipe(image())
+      .pipe(gulp.dest('./dist/images/'))
+})
+
+gulp.task('move', function(){
+  gulp.src(['./src/fonts/**'])
+      .pipe(plumber())      
+      .pipe(fileInclude({
+        prefix: '@@',
+        basepath: '@root'
+      }))
+      .pipe(gulp.dest('./dist/fonts/'))
+})
+
+gulp.task('serve', ['html','style','script','images','move'], function() {
 
   browserSync.init({
       server: "./dist"
@@ -50,6 +72,8 @@ gulp.task('serve', ['html','style','script'], function() {
 
   gulp.watch("./src/sass/**/*.scss", ['style']);
   gulp.watch("./src/**/*.html", ['html']);
+  gulp.watch("./src/images/**/**", ['images']);
+  gulp.watch("./src/fonts/**", ['move']);
 });
 
 // gulp.task('default', function () {
