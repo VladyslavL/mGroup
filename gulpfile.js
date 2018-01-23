58234de55a22c5732c5c5b122f6855c6c688086f
Vladyslav Liashenko <vladyslav.liashenko@gmail.com>
2018-01-23 11:08:08 +0200

    gulpfile is updated

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,18 @@ var gulp = require('gulp'),
 
 
 gulp.task('html', function() {
-  gulp.src(["./src/**/*.html", "!./src/fonts/**"])
+  gulp.src(["src/**/*.html", "!src/fonts/**"])
       .pipe(plumber())
       .pipe(fileInclude({
         prefix: '@@',
         basepath: '@root'
       }))
-      .pipe(gulp.dest('./dist/'))
+      .pipe(gulp.dest('dist/'))
       .pipe(browserSync.stream());
 })
 
 gulp.task('style', function () {
-  gulp.src(['./src/sass/*.scss'])
+  gulp.src(['src/sass/*.scss'])
       .pipe(plumber())
       .pipe(fileInclude({
         prefix: '@@',
@@ -35,49 +35,49 @@ gulp.task('style', function () {
       //   cascade: false
       // }))
       .pipe(sass())  
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist/css/'))
+      .pipe(sourcemaps.write(''))
+      .pipe(gulp.dest('dist/css/'))
       .pipe(browserSync.stream());
 });
 
 gulp.task('script', function() {
-  gulp.src(['./src/**/*.js'])
+  gulp.src(['src/**/*.js'])
       .pipe(plumber())
-      .pipe(gulp.dest('./dist/js/'))
+      .pipe(gulp.dest('dist/js/'))
       .pipe(browserSync.stream());
 })
 
 gulp.task('images', function(){
-  gulp.src(['./src/images/**/*'])
+  gulp.src(['src/images/**/*'])
       .pipe(plumber())
       .pipe(image())
-      .pipe(gulp.dest('./dist/images/'))
+      .pipe(gulp.dest('dist/images/'))
 })
 
 gulp.task('move', function(){
-  gulp.src(['./src/fonts/**/*'])
+  gulp.src(['src/fonts/**/*'])
       .pipe(plumber())      
       .pipe(fileInclude({
         prefix: '@@',
         basepath: '@root'
       }))
-      .pipe(gulp.dest('./dist/fonts/'))
+      .pipe(gulp.dest('dist/fonts/'))
 })
 
 gulp.task('serve', ['html','style','script','images','move'], function() {
 
   browserSync.init({
-      server: "./dist"
+      server: "dist"
   });
 
-  gulp.watch("./src/sass/**/*.scss", ['style']);
-  gulp.watch("./src/**/*.html", ['html']);
-  gulp.watch("./src/images/**/*.*", ['images']);
-  gulp.watch("./src/fonts/**/*.*", ['move']);
+  gulp.watch("src/sass/**/*.scss", ['style']);
+  gulp.watch("src/**/*.html", ['html']);
+  gulp.watch("src/images/**/*.*", ['images']);
+  gulp.watch("src/fonts/**/*.*", ['move']);
 });
 
 // gulp.task('default', function () {
-//   gulp.watch('./src/*.html', ['html']);
-//   gulp.watch('./src/sass/**/*.scss', ['style']);
-//   gulp.watch('./src/**/*.js', ['scripts']);
+//   gulp.watch('src/*.html', ['html']);
+//   gulp.watch('src/sass/**/*.scss', ['style']);
+//   gulp.watch('src/**/*.js', ['scripts']);
 // });
